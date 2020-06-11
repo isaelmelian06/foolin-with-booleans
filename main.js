b3a4703cf292c98a09e810bba3c0136e5def1eb8
Isael Melian <isael.melian@codeimmersives.com>
2020-06-11 14:20:10 -0400

    finished classwork

--- main.js
+++ main.js
@@ -2,77 +2,128 @@
  * YOUR CODE BELOW! *
  ********************/
 
+let iGetTheJoke = true;
+
+let havingFun = true;
+
+let learning = true;
+
+let killingIt = learning && havingFun;
+killingIt;
 
 
+function returnFalse(bool){
+  return false;
+  }
+  
+ function isOpposite(bool){
+   return !bool;
+ }
 
+ function both(bool1,bool2){
+   return bool1 && bool2;
+ }
 
-/********************************************************************************************
- * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
- *                              THIS IS FOR INTERNAL USE ONLY                               *
- *                          DON'T ADD TO OR CHANGE ANYTHING BELOW!                          *
- ********************************************************************************************/
+ function either(bool1,bool2){
+   return bool1 || bool2;
+ }
 
+ function firstOnly(bool1,bool2){
+   return bool1 && !bool2;
+ }
 
-if (typeof iGetTheJoke === 'undefined') {
-  iGetTheJoke = undefined;
+ function secondOnly(bool1,bool2){
+   return !bool1 && bool2;
+ }
+
+ function neither(bool1,bool2){
+  return !bool1 && !bool2;
 }
 
-if (typeof havingFun === 'undefined') {
-  havingFun = undefined;
-}
-
-if (typeof learning === 'undefined') {
-  learning = undefined;
-}
-
-if (typeof killingIt === 'undefined') {
-  killingIt = undefined;
-}
-
-if (typeof isOpposite === 'undefined') {
-  isOpposite = undefined;
-}
-
-if (typeof returnFalse === 'undefined') {
-  returnFalse = undefined;
-}
-
-if (typeof both === 'undefined') {
-  both = undefined;
-}
-
-if (typeof either === 'undefined') {
-  either = undefined;
-}
-
-if (typeof firstOnly === 'undefined') {
-  firstOnly = undefined;
-}
-
-if (typeof secondOnly === 'undefined') {
-  secondOnly = undefined;
-}
-
-if (typeof neither === 'undefined') {
-  neither = undefined;
-}
-
-if (typeof itsComplicated === 'undefined') {
-  itsComplicated = undefined;
+function itsComplicated(bool1,bool2,bool3){
+  return !bool1 || bool2 && bool3;
 }
 
 
-module.exports = {
-  iGetTheJoke,
-  havingFun,
-  learning,
-  killingIt,
-  returnFalse,
-  isOpposite,
-  both,
-  either,
-  firstOnly,
-  secondOnly,
-  neither,
-  itsComplicated,
-}
+
+
+
+
+
+
+
+
+
+
+
+
+  /********************************************************************************************
+   * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
+   *                              THIS IS FOR INTERNAL USE ONLY                               *
+   *                          DON'T ADD TO OR CHANGE ANYTHING BELOW!                          *
+   ********************************************************************************************/
+  
+  
+  if (typeof iGetTheJoke === 'undefined') {
+    iGetTheJoke = undefined;
+  }
+  
+  if (typeof havingFun === 'undefined') {
+    havingFun = undefined;
+  }
+  
+  if (typeof learning === 'undefined') {
+    learning = undefined;
+  }
+  
+  if (typeof killingIt === 'undefined') {
+    killingIt = undefined;
+  }
+  
+  if (typeof isOpposite === 'undefined') {
+    isOpposite = undefined;
+  }
+  
+  if (typeof returnFalse === 'undefined') {
+    returnFalse = undefined;
+  }
+  
+  if (typeof both === 'undefined') {
+    both = undefined;
+  }
+  
+  if (typeof either === 'undefined') {
+    either = undefined;
+  }
+  
+  if (typeof firstOnly === 'undefined') {
+    firstOnly = undefined;
+  }
+  
+  if (typeof secondOnly === 'undefined') {
+    secondOnly = undefined;
+  }
+  
+  if (typeof neither === 'undefined') {
+    neither = undefined;
+  }
+  
+  if (typeof itsComplicated === 'undefined') {
+    itsComplicated = undefined;
+  }
+  
+  
+  module.exports = {
+    iGetTheJoke,
+    havingFun,
+    learning,
+    killingIt,
+    returnFalse,
+    isOpposite,
+    both,
+    either,
+    firstOnly,
+    secondOnly,
+    neither,
+    itsComplicated,
+  }
